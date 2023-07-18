d27524401b24f7ee44fb83bfa4d661b23555cd3c
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-17 19:57:04 -0700

    MUI styling: add basic appbar component to header for now

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,40 @@
 import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import TransFlag from "./../img/transflag.png"
+import TransFlag2 from "./../img/transflag2.png"
+import TransFlag3 from "./../img/transflag3.png"
 
 function Header(){
   return (
     <React.Fragment>
-      <header>
-        <h1>TransGuide</h1>
-      </header>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <img src={TransFlag2}
+            height="30px"
+            alt="transgender flag"></img>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            TransGuide
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
     </React.Fragment>
   );
 }
