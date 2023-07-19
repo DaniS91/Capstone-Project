71d1f0ab6be6b98d35cb043a724e0f04974c8332
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-18 20:58:43 -0700

    attempt to add logic to the review form in order to create new review that will be concatenated to the review list property of the business object

---
+++ src/components/AddReviewForm.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReusableBusinessForm from "./ReusableBusinessForm";
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
+
+function AddReviewForm (props) {
+
+  const { business } = props;
+
+  // event handler
+  function handleAddReviewFormSubmission(event) {
+    event.preventDefault();
+    //i feel like I should create a review object from the inputs then concat the review to the reviewList that already exists in business???
+    //but i have no idea if this is how this works
+    //create review
+    let review = {
+      reviewTitle: event.target.reviewTitle.value,
+      rating: event.target.rating.value,
+      reviewText: event.target.rating.value,
+      businessId: business.id,
+      id: v4()
+    };
+    //send the review back to businesscontrol to add to the business??? idk man
+    onReviewBusiness(review);
+  }
+
+  return (
+    <React.Fragment>
+      <form onSubmit={handleAddReviewFormSubmission}>
+        <input
+          type='text'
+          name='reviewTitle'
+          placeholder='Your Review Title' />
+        <br></br>
+        <input 
+          type='text'
+          name='rating'
+          placeholder='Rating' />
+          <br></br>
+        <input 
+          type='text'
+          name='reviewText'
+          placeholder='You Review' />
+          <br></br>
+        <br></br>
+        <button type='submit'>Submit</button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+AddReviewForm.propTypes = {
+  business: PropTypes.object,
+  onReviewBusiness: PropTypes.func
+}
+export default AddReviewForm;
