099b7bbfdaa0bf5d7431ba618f161fe64824c2c5
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-23 11:05:15 -0700

    remove comments and unnecessary imports from header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,27 +4,17 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import TransFlag from "./../img/transflag.png"
 import TransFlag2 from "./../img/transflag2.png"
-import TransFlag3 from "./../img/transflag3.png"
+
 
 function Header(){
   return (
     <React.Fragment>
       <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar 
+        position="static"
+        >
         <Toolbar>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
           <img src={TransFlag2}
             height="30px"
             alt="transgender flag"></img>
