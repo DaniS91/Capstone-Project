d619f91d2f3a5caae3720b70fa795cffa24e7f46
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-22 17:09:07 -0700

    add dropdown menu of states for a better state input

--- src/components/ReusableBusinessForm.js
+++ src/components/ReusableBusinessForm.js
@@ -2,6 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ReusableBusinessForm(props) {
+
+const stateAbbreviations = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
+
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
@@ -19,11 +22,18 @@ function ReusableBusinessForm(props) {
           type='text'
           name='city'
           placeholder='City' />
-          <br></br>
-        <input 
-          type='text'
-          name='state'
-          placeholder='State' />
+        <br></br>
+          <label name="state">State:</label>
+          <select id="state" name="state">
+            <option value="">Select a state:</option>
+            {stateAbbreviations.map((stateAbbr) => (
+              <option key={stateAbbr} value={stateAbbr}>
+                {stateAbbr}
+              </option>
+            ))}
+          </select>
+        
+        <br></br>
           <br></br>
         <input 
           type='text'
