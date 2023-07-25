af8bc86a3e6f2a151f68ad8f76ad18fe6135b348
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-24 19:01:59 -0700

    add SignIn function to UserAccount component that will control signin, signup, signout

--- src/components/Auth/UserAccount.js
+++ src/components/Auth/UserAccount.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import SignUp from "./SignUp";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 function UserAccount(){  
   const [signUpSuccess, setSignUpSuccess] = useState(null);
+  const [signInSuccess, setSignInSuccess] = useState(null);
+
   function doSignUp(event) {
     event.preventDefault();
     const email = event.target.value;
@@ -17,9 +19,23 @@ function UserAccount(){
       });
   }
 
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+      setSignInSuccess(`You've successfully signed in as ${userCredential.user.email}!`)
+    })
+      .catch((error) => {
+      setSignInSuccess(`There was an error signing in: ${error.message}!`)
+    });
+  }
+
   return (
     <React.Fragment>
       <SignUp />
+      <SignIn />
     </React.Fragment>
   );
 }
