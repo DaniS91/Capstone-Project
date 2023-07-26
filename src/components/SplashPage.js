97d3314b02a87c16f37e79effdf9bb48261ed6bc
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-25 19:38:21 -0700

    add splash page for initial view before sign-in or sign-up that links to both

---
+++ src/components/SplashPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Typography, Box } from "@mui/material";
+
+function SplashPage(){
+
+  return (
+    <Box ss={{m:"10px"}}>
+      <Typography component="h1" variant="h5">Welcome to TransGuide!</Typography>
+    </Box>
+    
+
+  )
+}
+
+export default SplashPage;
