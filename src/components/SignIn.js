b7847a96aba97143bfe98a9f7c95cc9da6d86436
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-25 20:01:20 -0700

    add SignIn to SplashPage as main component, routes to signup if user doesn't have account

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -8,7 +8,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+// import Link from '@mui/material/Link';
+import { Link } from "react-router-dom";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -73,33 +74,21 @@ function SignIn(){
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
+              sx={{ mt: 3, mb: 2 }}>Sign In</Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link to="/SignUp">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
+        {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
       </Container>
     </React.Fragment>
   );
--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -1,12 +1,16 @@
 import React from "react";
+import SignIn from "./SignIn";
+import { Link } from "react-router-dom";
 
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Button } from "@mui/material";
 
 function SplashPage(){
 
   return (
-    <Box ss={{m:"10px"}}>
-      <Typography component="h1" variant="h5">Welcome to TransGuide!</Typography>
+    <Box>
+      <Typography component="h2" variant="h2">Welcome to TransGuide!</Typography>
+      <Typography variant="h4" component="h4" sx={{ flexGrow: 1 }}>Reviews</Typography>  
+      <SignIn />
     </Box>
     
 
