1b63d310fc2e5b16d18705901fc7ec9b2d43a2a2
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-27 19:57:02 -0700

    finally got firebase storage file upload and URL retrieval to work

--- src/components/NewBusinessForm.js
+++ src/components/NewBusinessForm.js
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import ReusableBusinessForm from "./ReusableBusinessForm";
 
+import { storage } from './../firebase.js';
+import { v4 } from "uuid";
+
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
 function NewBusinessForm(props){
 
-  function handleNewBusinessFormSubmission(event) {
+  const handleNewBusinessFormSubmission = async (event) => {
     event.preventDefault();
-    props.onNewBusinessCreation({
-      name: event.target.name.value,
-      address: event.target.address.value,
-      city: event.target.city.value,
-      state: event.target.state.value,
-      zipcode: parseInt(event.target.zipcode.value),
-      url: event.target.url.value,
-      category: event.target.category.value,
-      description: event.target.description.value,
-      accessibility: event.target.accessibility.checked,
-      restrooms: event.target.restrooms.checked,
-      // photo: event.target.file,
-      reviewList: [],
-      avgRating: null,
-    })
-  
-  }
+    const photoFile = event.target.photo.files;
+    const imageName = v4();
+    const imageRef = ref(storage, `images/${imageName}`); 
+    if (photoFile) {
+      await uploadBytes(imageRef, photoFile);
+      const photoURL = await getDownloadURL(imageRef);
+      console.log(photoURL);
+      props.onNewBusinessCreation({
+        name: event.target.name.value,
+        address: event.target.address.value,
+        city: event.target.city.value,
+        state: event.target.state.value,
+        zipcode: parseInt(event.target.zipcode.value),
+        url: event.target.url.value,
+        category: event.target.category.value,
+        description: event.target.description.value,
+        accessibility: event.target.accessibility.checked,
+        restrooms: event.target.restrooms.checked,
+        photo: photoURL,
+        reviewList: [],
+        avgRating: null,
+      });
+    } else {
+      props.onNewBusinessCreation({
+        name: event.target.name.value,
+        address: event.target.address.value,
+        city: event.target.city.value,
+        state: event.target.state.value,
+        zipcode: parseInt(event.target.zipcode.value),
+        url: event.target.url.value,
+        category: event.target.category.value,
+        description: event.target.description.value,
+        accessibility: event.target.accessibility.checked,
+        restrooms: event.target.restrooms.checked,
+        photo: null,
+        reviewList: [],
+        avgRating: null,
+      });
+    };
+  };
   return (
     <React.Fragment>
       <ReusableBusinessForm 
