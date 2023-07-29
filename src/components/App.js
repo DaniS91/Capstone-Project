fe151479135f572bb888040c26f96b7894eef333
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-29 13:13:19 -0700

    implement MUI custom theme provider to customize website look

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,56 @@ import BusinessControl from "./BusinessControl";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 function App(){
+  const theme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#231c35',
+        dark: '#070e48',
+      },
+      secondary: {
+        main: '#c690d4',
+      },
+      error: {
+        main: '#E74C3C',
+      },
+      warning: {
+        main: '#F7CA18',
+      },
+      info: {
+        main: '#3ca1af',
+      },
+      success: {
+        main: '#68C3A3',
+      },
+      background: {
+        default: '#e0e0e0',
+        paper: '#e0e0e0',
+      },
+    },
+    components: {
+      MuiButtonBase: {
+        defaultProps: {
+          margin: '10px',
+          disableRipple: true,
+        },
+      },
+    },
+  });
   return ( 
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/SignIn" element={<SignIn />} />
-        <Route path="/" element={<BusinessControl />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/" element={<BusinessControl />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
