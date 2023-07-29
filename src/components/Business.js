eed6f212b843fc4a1f4dfb9b980fb658777f6646
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-29 15:27:38 -0700

    MUI styling: add container styling to business item

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,20 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Rating from '@mui/material/Rating';
+import { Rating, Card } from '@mui/material/';
 
 
 function Business(props){
   return (
     <React.Fragment>
       
-      <div onClick = {() => props.whenBusinessClicked(props.id)}>
+      <Card 
+        variant="outlined"
+        onClick = {() => props.whenBusinessClicked(props.id)}>
         <h2>{props.name}</h2>
         {props.avgRating !== null ? (
           <Rating value={props.avgRating} readOnly={true}/>
         ) : (<p>Not yet Rated</p>)}
         <h3>{props.category}</h3>
         <p>{props.city}, {props.state}</p>
-      </div>
+      </Card>
       
     </React.Fragment>
   );
