ba2590b527125d145784b71362768132ecbc9ece
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-29 15:32:42 -0700

    add logo image and containers/spacing to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,20 @@
 import React from "react";
-
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-
+import { AppBar, Box, Toolbar, Typography } from '@mui/material/';
 import { Link } from "react-router-dom";
-import TransFlag2 from "./../img/transflag2.png"
 import TransFlag3 from "./../img/transflag3.png"
-
 
 function Header(){
   return (
     <React.Fragment>
       <Box sx={{ flexGrow: 1 }}>
-      <AppBar 
-        position="static"
-        >
+      <AppBar position="static">
         <Toolbar>
           <img src={TransFlag3}
             height="30px"
             alt="transgender flag"
             style={{margin:'10px'}}></img>
           <Link to="/" style={{textDecoration: "none"}}>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "whitesmoke" }}>
+            <Typography variant="h6" sx={{ flexGrow: 1, color: "whitesmoke" }}>
             TransGuide
             </Typography>
           </Link>
