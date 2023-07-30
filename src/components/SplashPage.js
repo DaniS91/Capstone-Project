227b9aae440d5e4584f7cf11a77ffbcd972739be
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-29 22:40:24 -0700

    add short blurb to splash page

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -6,9 +6,9 @@ import { Typography, Box} from "@mui/material";
 function SplashPage(){
 
   return (
-    <Box>
-      <Typography component="h2" variant="h2">Welcome to TransGuide!</Typography>
-      <Typography variant="h4" component="h4" sx={{ flexGrow: 1 }}>Reviews</Typography>  
+    <Box sx={{margin: '20px'}}>
+      <Typography component="h3" variant="h3">Welcome to TransGuide!</Typography>
+      <Typography variant="h4" component="h4" sx={{ flexGrow: 1 }}>Find accessible and inclusive businesses, while contributing to a comprehensive directory of businesses and reviews that fosters community empowerment. Help us build a supportive platform to share valuable resources within the community.</Typography>  
       <SignIn />
     </Box>
   )
